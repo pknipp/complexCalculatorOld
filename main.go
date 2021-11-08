dd409bfe4037731384f378acc5064bbcc2293d55
Peter Knipp <peteraknipp@gmail.com>
2021-11-07 20:22:03 -0500

    Make main a wrapper around "parseExpression", which'll allow recursing.

--- main.go
+++ main.go
@@ -7,23 +7,7 @@ import (
 	"math"
 )
 
-func getNumber(expression string) (float64, string){
-	p := 1
-	lastNum := 0.
-	// err := nil
-	for len(expression) >= p {
-		z := expression[0:p]
-		if !(z == "." || z == "-" || z == "-.") {
-			num, err := strconv.ParseFloat(z, 64)
-			if err != nil {
-				break
-			}
-			lastNum = num
-		}
-		p++
-	}
-	return lastNum, expression[p - 1:]
-}
+
 
 func calculate(x1 float64, op string, x2 float64) float64 {
 	result := 0.
@@ -43,52 +27,72 @@ func calculate(x1 float64, op string, x2 float64) float64 {
 }
 
 func main() {
-	type opNum struct {
-		op string
-		num float64
-	}
-	expression := " +23. / 78 ^ 2 + 4. * 6. - .5 "
-	expression = strings.ReplaceAll(expression, " ", "")
-	if expression[0:1] == "+" {
-		expression = expression[1:]
-	}
-	z := 0.
-	z, expression = getNumber(expression)
-	precedence := map[string]int{"+": 0, "-": 0, "*": 1, "/": 1, "^": 2}
-	pairs := []opNum{}
-	num := 0.
-	for len(expression) > 1 {
-		op := expression[0:1]
-		num, expression = getNumber(expression[1:])
-		pair := opNum{op, num}
-		pairs = append(pairs, pair)
-	}
-	for len(pairs) > 1 {
-		fmt.Println(z, pairs)
-		index := 0
-		for len(pairs) > index {
-			fmt.Println(index, z, pairs)
-			if index < len(pairs) - 1 && precedence[pairs[index].op] < precedence[pairs[index + 1].op] {
-				index++
-			} else {
-				x1 := 0.
-				if index == 0 {
-					x1 = z
-				} else {
-					x1 = pairs[index - 1].num
+	var expression string = " +23. / 78 ^ 2 + 4. * 6. - .5 "
+	parseExpression := func(expression string) (float64) {
+		getNumber := func(expression string) (float64, string){
+			p := 1
+			lastNum := 0.
+			// err := nil
+			for len(expression) >= p {
+				z := expression[0:p]
+				if !(z == "." || z == "-" || z == "-.") {
+					num, err := strconv.ParseFloat(z, 64)
+					if err != nil {
+						break
+					}
+					lastNum = num
 				}
-				fmt.Println(x1, pairs[index].op, pairs[index].num)
-				result := calculate(x1, pairs[index].op, pairs[index].num)
-				fmt.Println(result)
-				if index == 0 {
-					z = result
-					pairs = pairs[1:]
+				p++
+			}
+			return lastNum, expression[p - 1:]
+		}
+		type opNum struct {
+			op string
+			num float64
+		}
+
+		expression = strings.ReplaceAll(expression, " ", "")
+		if expression[0:1] == "+" {
+			expression = expression[1:]
+		}
+		z := 0.
+		z, expression = getNumber(expression)
+		precedence := map[string]int{"+": 0, "-": 0, "*": 1, "/": 1, "^": 2}
+		pairs := []opNum{}
+		num := 0.
+		for len(expression) > 1 {
+			op := expression[0:1]
+			num, expression = getNumber(expression[1:])
+			pair := opNum{op, num}
+			pairs = append(pairs, pair)
+		}
+		for len(pairs) > 1 {
+			// fmt.Println(z, pairs)
+			index := 0
+			for len(pairs) > index {
+				// fmt.Println(index, z, pairs)
+				if index < len(pairs) - 1 && precedence[pairs[index].op] < precedence[pairs[index + 1].op] {
+					index++
 				} else {
-					pairs[index - 1].num = result
-					pairs = append(pairs[0: index], pairs[index + 1:]...)
+					x1 := 0.
+					if index == 0 {
+						x1 = z
+					} else {
+						x1 = pairs[index - 1].num
+					}
+					result := calculate(x1, pairs[index].op, pairs[index].num)
+					if index == 0 {
+						z = result
+						pairs = pairs[1:]
+					} else {
+						pairs[index - 1].num = result
+						pairs = append(pairs[0: index], pairs[index + 1:]...)
+					}
+					index = 0
 				}
-				index = 0
 			}
 		}
+		return z
 	}
+	fmt.Println(parseExpression(expression));
 }
